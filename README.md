5a18ba981d107c7ff3c57768eac06aa9aa652b57
Pukimaa <pukima@pukima.site>
2022-04-10 21:58:12 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# `HelloAstro` Component
+
+This is a component that returns a random greeting...nothing more.
+
+### ❌ Something's missing or wrong?
+Please create a PR and follow these rules:
+- The translation should be just "Hello"
+- It should be the informal hello
+- Just create a new Array Value in the `greetings.js` like the following:
+    - `"Hello in Language X", // Language X Name`
---
+++ src/greetings.ts
@@ -0,0 +1,92 @@
+export const greetings: string[] = [
+    "Hello", // English & Bahaman
+    "Bonjour", // French
+    "Hola", // Spanish & Catalan
+    "Ciao", // Italian
+    "Olá", // Portuguese
+    "Kia ora", // Maori
+    "G'day", // Australian
+    "Geia", // Greek
+    "Zdravo", // Croatian
+    "Privet", // Russian
+    "Nǐ hǎo", // Cantonese
+    "Namaste", // Hindi & Nepali
+    "Kon'nichiwa", // Japanese
+    "Merhaba", // Turkish
+    "Anyeong", // Korean
+    "Ahoj", // Slovak & Czech
+    "Hallo", // German & Dutch
+    "Cześć", // Polish
+    "S̄wạs̄dī", // Thai
+    "Szia", // Hungarian
+    "Hyālō", // Bengali
+    "Assalam u Alaikum", // Urdu
+    "Salām", // Persian
+    "Dobryj Den", // Ukrainian
+    "Hallå", // Swedish
+    "Bunâ", // Romanian
+    "Shalom", // Hebrew
+    "Barev", // Armenian
+    "Marhabaan", // Arabic
+    "Sata Srī Akāla", // Punjabi
+    "halo", // Javanese
+    "Selamat Pagi", // Malay/Indonesian
+    "Xin Chào", // Vietnamese
+    "Kaixo", // Basque
+    "Servus", // Some parts of Germany
+    "Kamusta", // Tagalog
+    "Jambo", // Swahili
+    "Salom", // Uzbek
+    "Salam", // Azerbaijani & Pashto
+    "Slav", // Kurdish
+    "Bonjou", // Haitian Creole
+    "Dobry Dzień", // Belarusian
+    "Moni", // Chichewa
+    "Bula", // Fijian
+    "Aloha", // Hawaiian
+    "Tālofa", // Samoan
+    "Mālō e lelei", // Tongan
+    "A Gutn Tog", // Yiddish
+    "S'mae", // Welsh
+    "Hei", // Norwegian
+    "Sveiki", // Lithuanian
+    "Halló", // Icelandic
+    "Halò", // Scottish Gaelic
+    "Dia Duit", // Irish
+    "Tere", // Estonian
+    "Zdravo", // Bosnian
+    "Tashi Delek", // Tibetan
+    "Sabaideen", // Lao
+    "Gamarjoba", // Georgian
+    "Dumela", // Sesotho
+    "Saluton", // Esperanto
+    "Bongu", // Maltese
+    "Nde-ewo", // Igbo
+    "Moïen", // Luxembourgish
+    "Ya'at'eeh", // Navajo
+    "Grüezi", // Swiss German
+    "Ia Orana", // Tahitian
+    "Përshëndetje", // Albanian
+    "Ola", // Galician
+    "Sain uu", // Mongolian
+    "Salaam Aleekum", // Wolof
+    "Nóng gō", // Wu (Shanghainese)
+    "Vanakkam", // Tamil
+    "Namaskāra", // Kannada
+    "Sannu", // Hausa
+    "Mingalaba", // Burmese
+    "Selam", // Amharic
+    "Demat", // Breton
+    "Akkam", // Oromo
+    "Khurumjari", // Manipuri
+    "Kumusta", // Cebuano
+    "Salama", // Malagasy
+    "Henda ho", // Santali
+    "Choum Reap Sor", // Khmer
+    "Ayubowan", // Sinhalese
+    "Salam Alaykum", // Somali
+    "Li-hó", // Taiwanese Hokkien
+    "Dumela", // Tswana
+    "Allianchu", // Quechua
+    "Molweni", // Xhosa
+]
